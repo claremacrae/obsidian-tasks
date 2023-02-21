43757956edf3b97017ba6765ed1f00bac293324f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-21 19:20:30 +0000

    feat: Show Notice if fail to toggle a task

--- src/File.ts
+++ src/File.ts
@@ -1,4 +1,4 @@
-import { MetadataCache, TFile, Vault } from 'obsidian';
+import { MetadataCache, Notice, TFile, Vault } from 'obsidian';
 import type { ListItemCache } from 'obsidian';
 
 import { getSettings } from './Config/Settings';
@@ -146,14 +146,14 @@ const tryRepetitive = async ({
                 if (taskFromLine?.identicalTo(originalTask) === true) {
                     listItem = listItemCache;
                 } else {
-                    console.error(
-                        `Tasks: Unable to find task in file:
+                    const message = `Tasks: Unable to find task in file:
 ${originalTask.path}
 Expected task:
 ${originalTask.originalMarkdown}
 Found task line:
-${line}`,
-                    );
+${line}`;
+                    console.error(message);
+                    new Notice(message);
                     return;
                 }
                 break;
