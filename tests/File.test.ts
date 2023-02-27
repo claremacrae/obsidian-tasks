b7f02da43e4140cd2f30e1facff9924c626c424e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-27 21:52:27 +0000

    test: Group pairs of related tests together

--- tests/File.test.ts
+++ tests/File.test.ts
@@ -38,80 +38,108 @@ describe('File findLineNumberOfTaskToToggle()', () => {
 
     // --------------------------------------------------------------------------------
     // Issue 688
-    it.failing('should find line for block referenced task - issue 688', () => {
-        const jsonFileName = '688_toggle_block_referenced_line_overwrites_wrong_line.json';
-        const taskLineToToggle = '- [ ] #task task2b ^ca47c7';
-        // An incorrect line is currently found, so this test fails, due to bug 688
-        const expectedLineNumber = 10;
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
-    });
+    describe('should find line for block referenced task - issue 688', () => {
+        it.failing('should find line for block referenced task - issue 688', () => {
+            const jsonFileName = '688_toggle_block_referenced_line_overwrites_wrong_line.json';
+            const taskLineToToggle = '- [ ] #task task2b ^ca47c7';
+            // An incorrect line is currently found, so this test fails, due to bug 688
+            const expectedLineNumber = 10;
+            testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
+        });
 
-    it('should find line for block referenced task - issue 688 - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
-        const jsonFileName = '688_toggle_block_referenced_line_overwrites_wrong_line.json';
-        const taskLineToToggle = '- [ ] #task task2b ^ca47c7';
-        // An incorrect line is currently found, so this test fails, due to bug 688
-        const expectedLineNumber = 4;
-        const actualIncorrectLineFound = '- [ ] #task task1a';
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber, actualIncorrectLineFound);
+        it('should find line for block referenced task - issue 688 - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
+            const jsonFileName = '688_toggle_block_referenced_line_overwrites_wrong_line.json';
+            const taskLineToToggle = '- [ ] #task task2b ^ca47c7';
+            // An incorrect line is currently found, so this test fails, due to bug 688
+            const expectedLineNumber = 4;
+            const actualIncorrectLineFound = '- [ ] #task task1a';
+            testFindLineNumberOfTaskToToggle(
+                jsonFileName,
+                taskLineToToggle,
+                expectedLineNumber,
+                actualIncorrectLineFound,
+            );
+        });
     });
 
     // --------------------------------------------------------------------------------
     // when cache is out of date
-    it.failing('should not overwrite wrong line when cache is out of date', () => {
-        const jsonFileName = 'cache_is_out_of_date.json';
-        const taskLineToToggle = '- [ ] #task Heading 2/Task 1';
-        const expectedLineNumber = 15;
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
-    });
+    describe('should not overwrite wrong line when cache is out of date', () => {
+        it.failing('should not overwrite wrong line when cache is out of date', () => {
+            const jsonFileName = 'cache_is_out_of_date.json';
+            const taskLineToToggle = '- [ ] #task Heading 2/Task 1';
+            const expectedLineNumber = 15;
+            testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
+        });
 
-    it('should not overwrite wrong line when cache is out of date - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
-        const jsonFileName = 'cache_is_out_of_date.json';
-        const taskLineToToggle = '- [ ] #task Heading 2/Task 1';
-        const expectedLineNumber = 10;
-        /*
-        expected:
-        - [ ] #task Heading 2/Task 1
-        found:
-        - [ ] #task Heading 1/Task 1
-         */
-        const actualIncorrectLineFound = '- [ ] #task Heading 1/Task 1';
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber, actualIncorrectLineFound);
-    });
-
-    // --------------------------------------------------------------------------------
-    // when line does not exist - as the task had been toggled already,
-    // and the task in reading view had not yet been updated with the new markdown line.
-    it.failing('should not overwrite wrong line when line does not exist', () => {
-        const jsonFileName = 'line_does_not_exist.json';
-        const taskLineToToggle = '- [ ] #task y';
-        const expectedLineNumber = undefined;
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
-    });
-
-    it('should not overwrite wrong line when line does not exist - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
-        const jsonFileName = 'line_does_not_exist.json';
-        const taskLineToToggle = '- [ ] #task y';
-        const expectedLineNumber = 1;
-        const actualIncorrectLineFound = '- [x] #task y ✅ 2023-02-27';
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber, actualIncorrectLineFound);
+        it('should not overwrite wrong line when cache is out of date - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
+            const jsonFileName = 'cache_is_out_of_date.json';
+            const taskLineToToggle = '- [ ] #task Heading 2/Task 1';
+            const expectedLineNumber = 10;
+            /*
+            expected:
+            - [ ] #task Heading 2/Task 1
+            found:
+            - [ ] #task Heading 1/Task 1
+             */
+            const actualIncorrectLineFound = '- [ ] #task Heading 1/Task 1';
+            testFindLineNumberOfTaskToToggle(
+                jsonFileName,
+                taskLineToToggle,
+                expectedLineNumber,
+                actualIncorrectLineFound,
+            );
+        });
     });
 
     // --------------------------------------------------------------------------------
     // when line does not exist - as the task had been toggled already,
     // and the task in reading view had not yet been updated with the new markdown line.
-    it.failing('should not overwrite unindented line', () => {
-        const jsonFileName = 'line_was_indented_and_cache_not_yet_updated.json';
-        const taskLineToToggle = '    - [ ] #task task2c';
-        const expectedLineNumber = undefined;
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
+    describe('should not overwrite wrong line when line does not exist', () => {
+        it.failing('should not overwrite wrong line when line does not exist', () => {
+            const jsonFileName = 'line_does_not_exist.json';
+            const taskLineToToggle = '- [ ] #task y';
+            const expectedLineNumber = undefined;
+            testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
+        });
+
+        it('should not overwrite wrong line when line does not exist - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
+            const jsonFileName = 'line_does_not_exist.json';
+            const taskLineToToggle = '- [ ] #task y';
+            const expectedLineNumber = 1;
+            const actualIncorrectLineFound = '- [x] #task y ✅ 2023-02-27';
+            testFindLineNumberOfTaskToToggle(
+                jsonFileName,
+                taskLineToToggle,
+                expectedLineNumber,
+                actualIncorrectLineFound,
+            );
+        });
     });
 
-    it('should not overwrite unindented line - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
-        const jsonFileName = 'line_was_indented_and_cache_not_yet_updated.json';
-        const taskLineToToggle = '    - [ ] #task task2c';
-        const expectedLineNumber = 12;
-        const actualIncorrectLineFound = '- [ ] #task task2c';
-        testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber, actualIncorrectLineFound);
+    // --------------------------------------------------------------------------------
+    // when line does not exist - as the task had been toggled already,
+    // and the task in reading view had not yet been updated with the new markdown line.
+    describe('should not overwrite unindented line', () => {
+        it.failing('should not overwrite unindented line', () => {
+            const jsonFileName = 'line_was_indented_and_cache_not_yet_updated.json';
+            const taskLineToToggle = '    - [ ] #task task2c';
+            const expectedLineNumber = undefined;
+            testFindLineNumberOfTaskToToggle(jsonFileName, taskLineToToggle, expectedLineNumber);
+        });
+
+        it('should not overwrite unindented line - CURRENT BEHAVIOUR - WRONG RESULTS', () => {
+            const jsonFileName = 'line_was_indented_and_cache_not_yet_updated.json';
+            const taskLineToToggle = '    - [ ] #task task2c';
+            const expectedLineNumber = 12;
+            const actualIncorrectLineFound = '- [ ] #task task2c';
+            testFindLineNumberOfTaskToToggle(
+                jsonFileName,
+                taskLineToToggle,
+                expectedLineNumber,
+                actualIncorrectLineFound,
+            );
+        });
     });
     // --------------------------------------------------------------------------------
 });
