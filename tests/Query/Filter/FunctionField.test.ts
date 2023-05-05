b460497c700b40b923d81713747d3a122a77f2d1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-05-05 21:05:38 +0100

    test: Remove some repetition

--- tests/Query/Filter/FunctionField.test.ts
+++ tests/Query/Filter/FunctionField.test.ts
@@ -29,6 +29,12 @@ describe('FunctionField - grouping', () => {
         jest.useRealTimers();
     });
 
+    function createGrouper(line: string) {
+        const grouper = new FunctionField().createGrouperFromLine(line);
+        expect(grouper).not.toBeNull();
+        return grouper;
+    }
+
     it('should support grouping', () => {
         const field = new FunctionField();
         expect(field.supportsGrouping()).toEqual(true);
@@ -47,10 +53,7 @@ describe('FunctionField - grouping', () => {
 
     it('using root and path', () => {
         const line = 'group by function root === "journal/" ? root : path';
-        // TODO Remove repetition of these 3 lines
-        const field = new FunctionField();
-        const grouper = field.createGrouperFromLine(line);
-        expect(grouper).not.toBeNull();
+        const grouper = createGrouper(line);
 
         expect(grouper?.grouper(new TaskBuilder().path('journal/a/b').build())).toEqual(['journal/']);
         expect(grouper?.grouper(new TaskBuilder().path('hello/world/from-me.md').build())).toEqual([
@@ -61,18 +64,14 @@ describe('FunctionField - grouping', () => {
 
     it('using path stripping folder', () => {
         const line = 'group by function path.replace("some/prefix/", "")';
-        const field = new FunctionField();
-        const grouper = field.createGrouperFromLine(line);
-        expect(grouper).not.toBeNull();
+        const grouper = createGrouper(line);
 
         expect(grouper?.grouper(new TaskBuilder().path('a/b/c.md').build())).toEqual(['a/b/c']);
     });
 
     it('using due to group by month', () => {
         const line = 'group by function due ? "📅 " + due.format("YYYY-MM") : "no due date"';
-        const field = new FunctionField();
-        const grouper = field.createGrouperFromLine(line);
-        expect(grouper).not.toBeNull();
+        const grouper = createGrouper(line);
 
         expect(grouper?.grouper(new TaskBuilder().build())).toEqual(['no due date']);
         expect(grouper?.grouper(new TaskBuilder().dueDate('2023-01-23').build())).toEqual(['📅 2023-01']);
@@ -90,9 +89,7 @@ describe('FunctionField - grouping', () => {
         // TODO Really need to make this simpler to write
         const line =
             "group by function  (!due) ? 'No Due Date' : due.startOf('day').isBefore(moment().startOf('day')) ? 'Overdue' : due.startOf('day').isAfter(moment().startOf('day')) ? 'Future' : 'Today'";
-        const field = new FunctionField();
-        const grouper = field.createGrouperFromLine(line);
-        expect(grouper).not.toBeNull();
+        const grouper = createGrouper(line);
 
         expect(grouper?.grouper(new TaskBuilder().dueDate(yesdyString).build())).toEqual(['Overdue']);
         expect(grouper?.grouper(new TaskBuilder().dueDate(todayString).build())).toEqual(['Today']);
