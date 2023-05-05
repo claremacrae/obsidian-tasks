5209b5e5c1246efc098e5d1722e9c8ae1769ec7a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-05-05 11:07:07 +0100

    spike: Place-holder implementation of grouping by function

--- src/Query/Filter/FunctionField.ts
+++ src/Query/Filter/FunctionField.ts
@@ -1,3 +1,5 @@
+import type { Task } from '../../Task';
+import type { GrouperFunction } from '../Grouper';
 import { Field } from './Field';
 import { FilterOrErrorMessage } from './Filter';
 
@@ -7,10 +9,32 @@ export class FunctionField extends Field {
     }
 
     fieldName(): string {
-        return '';
+        return 'function';
     }
 
     protected filterRegExp(): RegExp | null {
         return null;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Grouping
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public supportsGrouping(): boolean {
+        return true;
+    }
+
+    protected grouperRegExp(): RegExp {
+        if (!this.supportsGrouping()) {
+            throw Error(`grouperRegExp() unimplemented for ${this.fieldNameSingular()}`);
+        }
+
+        return new RegExp(`^group by ${this.fieldNameSingularEscaped()}`);
+    }
+
+    public grouper(): GrouperFunction {
+        return (_task: Task) => {
+            return ['hello world'];
+        };
     }
 }
--- tests/Query/Filter/FunctionField.test.ts
+++ tests/Query/Filter/FunctionField.test.ts
@@ -1,10 +1,39 @@
 import { FunctionField } from '../../../src/Query/Filter/FunctionField';
+import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 
-describe('FunctionField', () => {
+describe('FunctionField - filter', () => {
     it('should not parse line', () => {
         const functionField = new FunctionField();
         const filterOrErrorMessage = functionField.createFilterOrErrorMessage('hello world');
         expect(filterOrErrorMessage).not.toBeValid();
         expect(filterOrErrorMessage.error).toStrictEqual('Searching by custom function not yet implemented');
+    });
+});
+
+describe('FunctionField - sorting', () => {
+    it('should not support sorting', () => {
+        const functionField = new FunctionField();
+        expect(functionField.supportsSorting()).toEqual(false);
+    });
+});
+
+describe('FunctionField - grouping', () => {
+    it('should support sorting', () => {
+        const field = new FunctionField();
+        expect(field.supportsGrouping()).toEqual(true);
+    });
+
+    it('should parse "group by function" line', () => {
+        // Arrange
+        const field = new FunctionField();
+        const instruction = 'group by function root === "journal/" ? root : path';
+
+        // Assert
+        expect(field.canCreateGrouperForLine(instruction)).toEqual(true);
+        const grouper = field.createGrouperFromLine(instruction);
+        expect(grouper).not.toBeNull();
+
+        const task = new TaskBuilder().build();
+        expect(grouper?.grouper(task)).toEqual(['hello world']);
     });
 });
