c963cdac7f41ede663abf51027d7b43e095dbf46
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-10-12 22:51:39 +0100

    spike: First attempt at generating tasks-api.d.ts - manually formatting

---
+++ src/api-entry.ts
@@ -0,0 +1,17 @@
+/**
+ * Public API entry point for Obsidian Tasks
+ *
+ * This file exports the types that should be available to users
+ * writing custom filters, groups, and sorts.
+ *
+ * Only members marked with @public in the source files will be
+ * included in the generated tasks-api.d.ts file.
+ */
+
+export { Task } from './Task/Task';
+export { ListItem } from './Task/ListItem';
+export { TasksFile } from './Scripting/TasksFile';
+export { TasksDate } from './DateTime/TasksDate';
+export { Link } from './Task/Link';
+export type { Status } from './Statuses/Status';
+export type { Priority } from './Task/Priority';
