c953e79bed52b1aa97845cd74b7cda20fdfa696d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-10-13 18:25:44 +0100

    vault: Access Tasks types in CodeScript Toolkit for JS

--- resources/sample_vaults/Tasks-Demo/_meta/codescript_toolkit_scripts/TasksFunctionsJS.js
+++ resources/sample_vaults/Tasks-Demo/_meta/codescript_toolkit_scripts/TasksFunctionsJS.js
@@ -1,3 +1,17 @@
+/**
+ * @typedef {import('../tasks-api').Task} Task
+ */
+
+/**
+ * @param {Task} task - the task being searched
+ */
 export function parentDescription(task) {
-    return task.description.toUpperCase();
+    return task.findClosestParentTask()?.description || 'No parent';
+}
+
+/**
+ * @param {Task} task - the task being searched
+ */
+export function statusName(task) {
+    return task.status.name;
 }
