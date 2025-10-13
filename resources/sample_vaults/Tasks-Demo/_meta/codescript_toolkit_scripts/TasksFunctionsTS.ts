45fcdce7d4e39b743b39afbdb1e866c4ba895507
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-10-13 18:25:23 +0100

    vault: Access Tasks types in CodeScript Toolkit for TS

--- resources/sample_vaults/Tasks-Demo/_meta/codescript_toolkit_scripts/TasksFunctionsTS.ts
+++ resources/sample_vaults/Tasks-Demo/_meta/codescript_toolkit_scripts/TasksFunctionsTS.ts
@@ -1,11 +1,9 @@
-interface Task {
-    description: string;
-}
+import type { Task } from '../tasks-api';
 
 // https://github.com/mnaoumov/obsidian-better-markdown-links
 // If you want to use the updated functions from your plugin, you can copy types.d.ts into your code.
 // https://github.com/mnaoumov/obsidian-better-markdown-links/blob/main/types.d.ts
 
 export function parentDescription(task: Task): string {
-    return task.description.toUpperCase();
+    return task.findClosestParentTask()?.description || 'No parent';
 }
